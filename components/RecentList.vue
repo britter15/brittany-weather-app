d9f2b016cca69046d5664eadabb4c9a3017f8c3f
Brittany <brittritt15@gmail.com>
2023-07-21 00:32:28 -0400

    onclick to card, update methods, fixing structure

--- components/RecentList.vue
+++ components/RecentList.vue
@@ -1,145 +1,35 @@
 <template>
   <section class="recent" elevation="0" v-if="recentList">
-    <ul class="no-bullets country-list">
-      <li>
-        <v-card class="recent-card pb-2">
-          <v-container>
-            <v-row no-gutters>
-              <v-col style="border: 1px solid #5096FF ; border-radius: 20px ;" class="mb-2 pa-2" cols="12"
-                v-for="country in cities" :key="country">
-                <h1 class="recent-card__title" @click="getDetail(country)">{{ country.name }}</h1>
-                <p class="recent-card__degree set_low"> {{ country.main.temp }} &deg;</p>
-                <p class="recent-card__time">{{ country.main.time }}</p>
-
-                <p class="recent-card__description">Mostly {{ country.weather[0].main }}</p>
-                <p class="recent-card__range set_low" style="padding-top: 10px !important;">High: {{ Math.round(country.main.temp_max - 273) }} &deg;
-                  Low: {{ Math.round(country.main.temp_min - 273) }} &deg;</p>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card>
+    <ul>
+      <li v-for="(data, index) in recentList" :key="index">
+        <v-card class="recent-card" @click="onCardClick(index)">
+          <div class="address-info">
+            <h1 class="recent-card__title">{{ data.name }}</h1>
+            <p class="recent-card__time">{{ $moment.unix(data.dt).format("hh:mm A") }}</p>
+            <p class="recent-card__description">{{ data.weather[0].__description }}</p>
+          </div>
+          <div class="weather-info">
+            <p class="recent-card__degree">{{ Math.round(data.main.temp- 273.15) }}&deg;</p>
+            <p class="recent-card__range"> High: {{ Math.round(data.main.temp_max - 273.15) }}&deg; Low: {{ Math.round(data.main.temp_min - 273.15) }}&deg;
+            </p>
+          </div>
+      </v-card>
       </li>
     </ul>
   </section>
 </template>
 <script>
 export default {
-  name: 'RecentList',
-  data() {
-    return {
-      states: [],
-      cities: [
-        {
-          name:'Tarpon Springs',
-          main:{
-            time : '11:22 AM',
-            temp : 30,
-            temp_min : 300 ,
-            temp_max : 320
-          },
-          weather: [
-            {
-              main :'Sunny'
-            }
-          ]
-        },
-        {
-          name: 'Denver',
-          main: {
-            time : '9:22 AM',
-            temp: 52,
-            temp_min: 300,
-            temp_max: 320
-          },
-          weather: [
-            {
-              main: 'Cloudy'
-            }
-          ]
-        },
-        {
-          name: 'Tampa',
-          main: {
-            time : '11:22 AM',
-            temp: 32,
-            temp_min: 300,
-            temp_max: 320
-          },
-          weather: [
-            {
-              main: 'Cloudy'
-            }
-          ]
-        },
-        {
-          name: 'Jacksonville',
-          main: {
-            time : '11:22 AM',
-            temp: 34,
-            temp_min: 300,
-            temp_max: 320
-          },
-          weather: [
-            {
-              main: 'Cloudy'
-            }
-          ]
-        },
-        {
-          name: 'Denver',
-          main: {
-            time : '9:22 AM',
-            temp: 52,
-            temp_min: 300,
-            temp_max: 320
-          },
-          weather: [
-            {
-              main: 'Cloudy'
-            }
-          ]
-        },
-        // {
-        //   name: 'Tampa',
-        //   main: {
-        //     temp: 32,
-        //     temp_min: 300,
-        //     temp_max: 320
-        //   },
-        //   weather: [
-        //     {
-        //       main: 'Cloudy'
-        //     }
-        //   ]
-        // },
-      ], 
-    }
+  computed: {
+    recentList() {
+      return this.$store.getters.getRecentList;
+    },
   },
   methods: {
-    async getWeather() {
-      this.states = this.$store.state.states;
-      this.states.forEach((value) => {
-        fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + value.lat + "&lon=" + value.lon + "&appid=b8f5d5a3c8c40a270978a5686d277fbd", { method: "GET" })
-          .then((response) => {
-            response.json().then((data) => {
-              // console.log(data)
-              this.cities.push(data)
-            });
-          })
-          .catch((err) => {
-            console.error(err);
-          });
-      });
-    },
-    getDetail(name){
-      // here is country name is country detail
-      this.$store.state.show_detail = name ;
+    onCardClick(index) {
+      this.$store.dispatch("updateCardSelection", this.recentList.length - index - 1);
     }
   },
-  watch: {
-    "$store.state.search_city": "getWeather",
-  },
-
 }
 </script>
 
@@ -158,7 +48,6 @@ export default {
     text-align: left;
     color: #ffffff;
     margin-bottom: 3px;
-
     width: 50%;
     display: inline-block;
     padding-top: 15px !important;
@@ -217,6 +106,4 @@ export default {
   width: 90%;
   display: inline-block;
 }
-
-
 </style>
